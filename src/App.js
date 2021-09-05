2694359e9589359b28b79b6140304371a7f298bb
yonatana <yonatana@jfrog.com>
2021-09-05 17:00:32 +0300

    added guyda

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Welcome to your first CI/CD Expirience
+        Guy David
         </p>
         <a
           className="App-link"
